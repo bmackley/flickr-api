d8257a7cbf045a2f21373537948662f36f658b8b
Ben Mackley <benjaminmackley@gmail.com>
2018-11-13 14:15:35 -0500

    updated styling. Now shows in cards and is somewhat responsive.

--- src/Content.js
+++ src/Content.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import Parser from 'html-react-parser';
+import './Content.css'
+
 class Content extends Component {
   constructor() {
     super();
-    this.state = { data: {}, photos: []};
+    this.state = {photos: []};
   }
   async componentDidMount() {
-    const response = await fetch(`https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=a5e95177da353f58113fd60296e1d250&user_id=24662369@N07&format=json&nojsoncallback=2`);
+    const response = await fetch(`https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=a5e95177da353f58113fd60296e1d250&user_id=24662369@N07&extras=description&format=json&nojsoncallback=2&per_page=10&page=2`);
     const jsonResponse = await response.json();
+    console.log(jsonResponse)
     const photosWithUrls = jsonResponse.photos.photo.map((photo)=>{
       const url = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
       photo.url = url;
@@ -20,7 +23,11 @@ class Content extends Component {
     return (
       <div className="contentArea">
       {this.state.photos.map((photo) =>
-          <img key={photo.id} src= {photo.url} />
+          <div key={photo.id} className="photo-card">
+            <img src= {photo.url} alt="title" />
+            <figcaption className="image-title">{photo.title} </figcaption>
+            <p className="image-description">{Parser(photo.description._content)}</p>
+          </div>
        )}
       </div>
     );
